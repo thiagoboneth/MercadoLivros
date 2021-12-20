f79cd6e6e782c029b5ecf3c944e3deffdc578adc
thiagoboneth <thiago.luis@mercadolivre.com>
2021-12-20 18:57:53 -0300

    Importação do mysql corrigida

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,10 +16,12 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("mysql:mysql-connector-java:8.0.25")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
