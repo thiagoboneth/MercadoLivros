2ea8807639141d7283a36f30de08d214927bc382
thiagoboneth <thiago.luis@mercadolivre.com>
2021-12-22 18:19:06 -0300

    Implementado delete de customer que muda o status de seus livros comprados para deleted

--- src/main/kotlin/com/mercadolivre/mercadolivros/controller/BookController.kt
+++ src/main/kotlin/com/mercadolivre/mercadolivros/controller/BookController.kt
@@ -1,6 +1,7 @@
 package com.mercadolivre.mercadolivros.controller
 
 import com.mercadolivre.mercadolivros.controller.request.PostBookRequest
+import com.mercadolivre.mercadolivros.controller.request.PutBookRequest
 import com.mercadolivre.mercadolivros.extension.toBookModel
 import com.mercadolivre.mercadolivros.model.BookModel
 import com.mercadolivre.mercadolivros.service.BookService
@@ -19,7 +20,7 @@ class BookController(
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody request: PostBookRequest) {
-        val customer = customerService.getById(request.customerId)
+        val customer = customerService.findById(request.customerId)
         bookService.create(request.toBookModel(customer))
     }
 
@@ -35,4 +36,22 @@ class BookController(
         return bookService.findActives()
     }
 
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    fun findById(@PathVariable id:Int): BookModel{
+        return bookService.findById(id)
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun deleteById(@PathVariable id:Int) {
+        bookService.delete(id)
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun updateById(@PathVariable id:Int, @RequestBody book: PutBookRequest) {
+        val bookSaved = bookService.findById(id)
+        bookService.updateById(book.toBookModel(bookSaved))
+    }
 }
