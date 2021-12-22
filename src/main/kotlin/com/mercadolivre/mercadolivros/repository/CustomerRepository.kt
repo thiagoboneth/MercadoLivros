6f59a6058171859db209fe0c165e550613eee0c6
thiagoboneth <thiago.luis@mercadolivre.com>
2021-12-22 18:20:43 -0300

    refatorção de linha

--- src/main/kotlin/com/mercadolivre/mercadolivros/repository/CustomerRepository.kt
+++ src/main/kotlin/com/mercadolivre/mercadolivros/repository/CustomerRepository.kt
@@ -5,4 +5,5 @@ import org.springframework.data.repository.CrudRepository
 
 interface CustomerRepository: CrudRepository<CustomerModel, Int>{
     fun findByName(name: String): List<CustomerModel>
+
 }
