52efab29cd74f5d7c1e92b016e7daf647d5738ce
thiagoboneth <thiago.luis@mercadolivre.com>
2021-12-22 18:21:06 -0300

    Implementação do service do book model

--- src/main/kotlin/com/mercadolivre/mercadolivros/service/BookService.kt
+++ src/main/kotlin/com/mercadolivre/mercadolivros/service/BookService.kt
@@ -1,8 +1,8 @@
 package com.mercadolivre.mercadolivros.service
 
-import com.mercadolivre.mercadolivros.controller.request.PostBookRequest
 import com.mercadolivre.mercadolivros.enums.BookStatus
 import com.mercadolivre.mercadolivros.model.BookModel
+import com.mercadolivre.mercadolivros.model.CustomerModel
 import com.mercadolivre.mercadolivros.repository.BookRepository
 import org.springframework.stereotype.Service
 
@@ -22,5 +22,27 @@ class BookService (
     fun findActives(): List<BookModel> {
         return bookRepository.findByStatus(BookStatus.ATIVO)
 
+    }
+
+    fun findById(id: Int): BookModel {
+        return bookRepository.findById(id).orElseThrow()
+    }
+
+    fun delete(id: Int) {
+        var book = findById(id)
+        book.status = BookStatus.CANCELADO
+        updateById(book)
+    }
+
+    fun updateById(book: BookModel) {
+        bookRepository.save(book)
+    }
+
+    fun deleteByCustomer(customer: CustomerModel) {
+        var books = bookRepository.findByCustomer(customer)
+        for (book in books){
+            book.status = BookStatus.DELETADO
+        }
+        bookRepository.saveAll(books)
     }
 }
