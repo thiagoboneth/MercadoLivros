842e508aa931a34cfcaaf125b6a7fc2b9a91e8a5
thiagoboneth <thiago.luis@mercadolivre.com>
2021-12-22 18:20:25 -0300

    implementação de findbycustomer para trazer informação do dono dos livros junto dos livros

--- src/main/kotlin/com/mercadolivre/mercadolivros/repository/BookRepository.kt
+++ src/main/kotlin/com/mercadolivre/mercadolivros/repository/BookRepository.kt
@@ -1,11 +1,11 @@
 package com.mercadolivre.mercadolivros.repository
 
-import com.mercadolivre.mercadolivros.controller.request.PostBookRequest
 import com.mercadolivre.mercadolivros.enums.BookStatus
 import com.mercadolivre.mercadolivros.model.BookModel
+import com.mercadolivre.mercadolivros.model.CustomerModel
 import org.springframework.data.repository.CrudRepository
 
 interface BookRepository: CrudRepository<BookModel, Int>{
-    fun findByName(name: PostBookRequest): List<BookModel>
     fun findByStatus(ativo: BookStatus): List<BookModel>
+    fun findByCustomer(customer: CustomerModel): List<BookModel>
 }
