08996650e07ad5622b12348a1535348bb85ec05f
thiagoboneth <thiago.luis@mercadolivre.com>
2021-12-22 15:28:03 -0300

    request de book implementado "DTO"

--- src/main/kotlin/com/mercadolivre/mercadolivros/extension/ConverterExtensionFunction.kt
+++ src/main/kotlin/com/mercadolivre/mercadolivros/extension/ConverterExtensionFunction.kt
@@ -1,12 +1,22 @@
 package com.mercadolivre.mercadolivros.extension
 
+import com.mercadolivre.mercadolivros.controller.request.PostBookRequest
 import com.mercadolivre.mercadolivros.controller.request.PostCustomerRequest
 import com.mercadolivre.mercadolivros.controller.request.PutCustomerRequest
+import com.mercadolivre.mercadolivros.enums.BookStatus
+import com.mercadolivre.mercadolivros.model.BookModel
 import com.mercadolivre.mercadolivros.model.CustomerModel
+import java.util.*
 
 fun PostCustomerRequest.toCustomerModel(): CustomerModel {
     return CustomerModel(name = this.name, email = this.email)
 }
-fun PutCustomerRequest.toCustomerModel(id: String): CustomerModel {
+fun PutCustomerRequest.toCustomerModel(id: Int): CustomerModel {
     return CustomerModel(id = id, name = this.name, email = this.email)
+}
+
+fun PostBookRequest.toBookModel(customer: CustomerModel): BookModel {
+    return BookModel(
+        name = this.name, price = this.price, status = BookStatus.ATIVO, customer = customer
+    )
 }
