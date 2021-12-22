2673f001d2426e9e55a9c31b32cbf61dbfa1ae7a
thiagoboneth <thiago.luis@mercadolivre.com>
2021-12-22 18:19:52 -0300

    implementação de put request para converter dto

--- src/main/kotlin/com/mercadolivre/mercadolivros/extension/ConverterExtensionFunction.kt
+++ src/main/kotlin/com/mercadolivre/mercadolivros/extension/ConverterExtensionFunction.kt
@@ -2,6 +2,7 @@ package com.mercadolivre.mercadolivros.extension
 
 import com.mercadolivre.mercadolivros.controller.request.PostBookRequest
 import com.mercadolivre.mercadolivros.controller.request.PostCustomerRequest
+import com.mercadolivre.mercadolivros.controller.request.PutBookRequest
 import com.mercadolivre.mercadolivros.controller.request.PutCustomerRequest
 import com.mercadolivre.mercadolivros.enums.BookStatus
 import com.mercadolivre.mercadolivros.model.BookModel
@@ -20,3 +21,10 @@ fun PostBookRequest.toBookModel(customer: CustomerModel): BookModel {
         name = this.name, price = this.price, status = BookStatus.ATIVO, customer = customer
     )
 }
+
+fun PutBookRequest.toBookModel(book: BookModel): BookModel {
+    return BookModel(
+        id = book.id, name = this.name?:book.name, price = this.price?:book.price, status = book.status, customer = book.customer
+    )
+}
+
